6406316129a0ac060de80aabedb3fee7cd9068e1
Exzeed <geethan1996@hotmail.ca>
2020-11-13 17:58:08 -0500

    Coding of Post Job Page
    has the form used for Posting Jobs

---
+++ PerfectBabysitter/Controllers/PostJobController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PerfectBabysitter.Models;
+
+namespace PerfectBabysitter.Controllers
+{
+    public class PostJobController : Controller
+    {
+        [HttpGet]
+        public ViewResult JobForm()
+        {
+            JobPosting job = new JobPosting();
+            return View(job);
+        }
+
+        [HttpPost]
+        public ActionResult JobForm(JobPosting job)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(CompletedForm));
+            }
+
+            //return RedirectToAction(nameof(CompletedForm));
+            return View(job);
+        }
+
+        public ViewResult CompletedForm()
+        {
+            ViewBag.CompleteMessage = "Your job posting has been uploaded.";
+            return View();
+        }
+    }
+}
---
+++ PerfectBabysitter/Models/JobPosting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+namespace PerfectBabysitter.Models
+{
+    public class JobPosting
+    {
+        [Required(ErrorMessage = "Please enter a phone number")]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        public string PhoneNumber {get;set;}
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email {get;set;}
+        [Required(ErrorMessage = "Please enter an address")]
+        public string Address {get;set;}
+        [Required(ErrorMessage = "Please specify the number of children to be looked after")]
+        public string Children{get;set;}
+        [Required(ErrorMessage = "Please enter when your scheduling for")]
+        public string Date{get;set;}
+        [Required(ErrorMessage = "Please enter time to start")]
+        public string StartTime{get;set;}
+        [Required(ErrorMessage = "Please enter time to end")]
+        public string EndTime{get;set;}
+        [Required(ErrorMessage = "Please enter your preferred choice of language")]
+        public string Language{get;set;}
+        public string Description{get;set;}
+    }
+}
