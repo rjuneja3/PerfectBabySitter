4b406e21ba0ff69f0446ea08dff88d01cc86d175
Exzeed <geethan1996@hotmail.ca>
2020-11-17 16:23:55 -0500

    Update + Validation
    Updated the input fields for users to be more tailored to what they should be entering.
    Added functional validation that would transfer to from completion should there be no errors

--- PerfectBabysitter/Models/JobPosting.cs
+++ PerfectBabysitter/Models/JobPosting.cs
@@ -11,20 +11,29 @@ namespace PerfectBabysitter.Models
         [Required(ErrorMessage = "Please enter a phone number")]
         [Phone(ErrorMessage = "Please enter a valid phone number")]
         public string PhoneNumber {get;set;}
+
+        [Required(ErrorMessage = "Please enter a email")]
         [EmailAddress(ErrorMessage = "Please enter a valid email")]
         public string Email {get;set;}
+
         [Required(ErrorMessage = "Please enter an address")]
         public string Address {get;set;}
+
         [Required(ErrorMessage = "Please specify the number of children to be looked after")]
-        public string Children{get;set;}
+        public int Children{get;set;}
+
         [Required(ErrorMessage = "Please enter when your scheduling for")]
         public string Date{get;set;}
-        [Required(ErrorMessage = "Please enter time to start")]
+
+        [Required(ErrorMessage = "Please enter time to start (include am or pm)")]
         public string StartTime{get;set;}
-        [Required(ErrorMessage = "Please enter time to end")]
+
+        [Required(ErrorMessage = "Please enter time to end (include am or pm)")]
         public string EndTime{get;set;}
+
         [Required(ErrorMessage = "Please enter your preferred choice of language")]
         public string Language{get;set;}
+
         public string Description{get;set;}
     }
 }
