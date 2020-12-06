50366d0bd8a908c6dc5f78bb7b017e3efeb97c29
Rohan Juneja <rjuneja3@my.centennialcollege.ca>
2020-12-06 18:15:24 -0500

    Added Shared layout pages for Separate Account type - Parent/Babysitter

---
+++ PerfectBabysitter/Migrations/20201206220226_UpdatedFileType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PerfectBabysitter.Migrations
+{
+    public partial class UpdatedFileType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Resume",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Resume",
+                table: "Accounts",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
